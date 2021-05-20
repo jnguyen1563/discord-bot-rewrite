b3310714b62258d7d674a5900f4a384679f9ba4d
jgnuyen1563 <justinnguyen0821@gmail.com>
2021-05-20 00:23:42 -0700

    Added sql to create database and tables

--- init_db.py
+++ init_db.py
@@ -1,7 +1,40 @@
 import sqlite3
+from sqlite3 import Error
 
-def create_table():
+def create_connection(db):
+    """ Create a connection to SQLite database, create a database if not exists """
+    conn = None
+    try:
+        conn = sqlite3.connect(db)
+        return conn
+    except Error as e:
+        print(e)
+
+    return conn
+
+def create_table(conn, table_sql):
     """
     Create the user table for SQLite database
     """
-    pass
+    try:
+        c = conn.cursor()
+        c.execute(table_sql)
+    except Error as e:
+        print(e)
+
+def main():
+    conn = create_connection(r'./databases/balances.db')
+
+    table_sql = """
+    CREATE TABLE IF NOT EXISTS users (
+        id integer PRIMARY KEY,
+        balance integer
+    );"""
+
+    if conn:
+        create_table(conn, table_sql)
+    else:
+        print("Cannot create database")
+
+if __name__ == '__main__':
+    main()
