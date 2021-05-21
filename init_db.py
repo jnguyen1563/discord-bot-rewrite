f705acd89fe3fac4215ed6683c9c169048764f9f
jgnuyen1563 <justinnguyen0821@gmail.com>
2021-05-21 01:12:25 -0700

    Simplified the program

--- init_db.py
+++ init_db.py
@@ -1,40 +1,32 @@
 import sqlite3
 from sqlite3 import Error
 
-def create_connection(db):
-    """ Create a connection to SQLite database, create a database if not exists """
+def create_database(table_name, table_sql):
+    """
+    Create a SQLite database
+    :param table_name: The name used for the database table
+    :param table_sql: The SQL commands used to create the table
+    """
     conn = None
     try:
-        conn = sqlite3.connect(db)
-        return conn
-    except Error as e:
-        print(e)
-
-    return conn
-
-def create_table(conn, table_sql):
-    """
-    Create the user table for SQLite database
-    """
-    try:
+        conn = sqlite3.connect(fr'./databases/{table_name}.db')
         c = conn.cursor()
         c.execute(table_sql)
+        print(f'Sucessfully created table {table_name}')
     except Error as e:
+        print(f'Failed to create table {table_name}')
         print(e)
 
 def main():
-    conn = create_connection(r'./databases/balances.db')
 
-    table_sql = """
+    balances_sql = """
     CREATE TABLE IF NOT EXISTS users (
         id integer PRIMARY KEY,
-        balance integer
+        balance real,
+        crypto real
     );"""
 
-    if conn:
-        create_table(conn, table_sql)
-    else:
-        print("Cannot create database")
+    create_database('balances', balances_sql)
 
 if __name__ == '__main__':
     main()
