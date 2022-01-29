cdbb411b57074d956d240ffaab6e8a323b8d2722
Justin Nguyen <justinnguyen0821@gmail.com>
2022-01-29 01:25:50 -0800

    Converted code to boilerplate Typescript

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const word = 'world';
+function hello(world = word) {
+    return `Hello ${world}!`;
+}
+exports.default = hello;
+//# sourceMappingURL=index.js.map
